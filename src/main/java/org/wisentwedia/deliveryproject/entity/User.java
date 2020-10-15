ca45ae1e4f6e90e37335dbdbbea91b1f511a1748
alinka241199@gmail.com <medvdv_a@gmail.com>
2020-10-15 23:09:54 +0300

    edit test

--- src/main/java/org/wisentwedia/deliveryproject/entity/User.java
+++ src/main/java/org/wisentwedia/deliveryproject/entity/User.java
@@ -18,13 +18,11 @@ public class User {
     private long id;
 
     private String name;
-    private String email;
 
     public User() {
     }
 
     public User(String name, String email) {
         this.name = name;
-        this.email = email;
     }
 }
